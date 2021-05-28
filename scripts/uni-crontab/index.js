ce256c067deb7fc6f7e5db230f94fb4a809d00db
Donjan <Donjan@cqbaobao.cn>
2021-05-28 15:47:27 +0800

    超时设置

--- scripts/uni-crontab/index.js
+++ scripts/uni-crontab/index.js
@@ -1,5 +1,11 @@
 const axios = require('axios');
 (async () => {
-    let res = await axios.get(`https://74685ec4-fb0e-458f-a4a6-19264065d9f7.bspapp.com/http/helper?action=crontab-mint&appkey=` + process.env.APP_KEY);
+    let res = await axios.get(`https://74685ec4-fb0e-458f-a4a6-19264065d9f7.bspapp.com/http/helper`, {
+        params: {
+            action: 'action=crontab-mint',
+            appkey: process.env.APP_KEY
+        },
+        timeout: 1000 * 60
+    });
     console.log(res);
 })();
