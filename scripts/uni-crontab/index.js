0e32cee71c17025e4cb16a7e7553d2b320167491
Donjan <Donjan@cqbaobao.cn>
2021-05-28 15:53:44 +0800

    参数

--- scripts/uni-crontab/index.js
+++ scripts/uni-crontab/index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 (async () => {
     let res = await axios.get(`https://74685ec4-fb0e-458f-a4a6-19264065d9f7.bspapp.com/http/helper`, {
         params: {
-            action: 'action=crontab-mint',
+            action: 'crontab-mint',
             appkey: process.env.APP_KEY
         },
         timeout: 1000 * 60
